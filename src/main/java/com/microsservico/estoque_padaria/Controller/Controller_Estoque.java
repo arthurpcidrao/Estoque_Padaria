9652fd5d29e7daecb134aa596f9fca4e39620d6c
Arthur Paraiba Cidrao <arthurcidrao@sas.com.br>
2024-11-28 10:20:34 -0300

    configurando o controller do estoque

--- src/main/java/com/microsservico/estoque_padaria/Controller/Controller_Estoque.java
+++ src/main/java/com/microsservico/estoque_padaria/Controller/Controller_Estoque.java
@@ -1,4 +1,27 @@
 package com.microsservico.estoque_padaria.Controller;
 
+import com.microsservico.estoque_padaria.Model.Estoque;
+import com.microsservico.estoque_padaria.Repository.Estoque_DAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/estoque")
 public class Controller_Estoque {
+    // http://localhost:8080/usuario
+    @Autowired
+    private Estoque_DAO dao;
+
+    @GetMapping("/todos")
+    public List<Estoque> listaEstoque(){
+        return (List<Estoque>) dao.findAll();
+    }
+
+    @PutMapping("/editar")
+    public Estoque editarEstoque(@RequestBody Estoque estoque){
+        return dao.save(estoque);
+    }
 }
