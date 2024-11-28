25966e56d45d29167623a8cdf1d556c4259f07b1
Arthur Paraiba Cidrao <arthurcidrao@sas.com.br>
2024-11-28 20:34:30 -0300

    Métodos para comunicação via rede no Controller_Estoque

--- src/main/java/com/microsservico/estoque_padaria/Controller/Controller_Estoque.java
+++ src/main/java/com/microsservico/estoque_padaria/Controller/Controller_Estoque.java
@@ -3,15 +3,17 @@ package com.microsservico.estoque_padaria.Controller;
 import com.microsservico.estoque_padaria.Model.Estoque;
 import com.microsservico.estoque_padaria.Repository.Estoque_DAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/estoque")
 public class Controller_Estoque {
-    // http://localhost:8080/usuario
+    // http://localhost:8080/estoque
     @Autowired
     private Estoque_DAO dao;
 
@@ -20,8 +22,33 @@ public class Controller_Estoque {
         return (List<Estoque>) dao.findAll();
     }
 
+    @GetMapping("/listaprodutos")
+    public List<Estoque> listaEstoqueProdutos(@RequestParam List<Integer> ids){
+        return (List<Estoque>) dao.findAllById(ids);
+    }
+
+    @GetMapping("/{produto}")
+    public Optional<Estoque> buscaProduto(@PathVariable int produto){  //Optional -> se não encontrar, retorna NULL
+        return dao.findById(produto);
+    }
+
+    @PostMapping("/vendeu")
+    public ResponseEntity<String> atualizarEstoque(@RequestBody List<Estoque> produtoVendido) {
+        for (Estoque produto : produtoVendido) {
+            Optional<Estoque> optionalProdutoEstoque = dao.findById(produto.getIdProduto());
+
+            if (optionalProdutoEstoque.isPresent()) {
+                Estoque produtoEstoque = optionalProdutoEstoque.get();
+                produtoEstoque.setQuantidade(produtoEstoque.getQuantidade() - produto.getQuantidade());
+                dao.save(produtoEstoque);
+            }
+        }
+        return ResponseEntity.ok("Estoque atualizado com sucesso!");
+    }
+
+
     @PutMapping("/editar")
-    public Estoque editarEstoque(@RequestBody Estoque estoque){
-        return dao.save(estoque);
+    public Estoque editarEstoque(@RequestBody Estoque produto){
+        return dao.save(produto);
     }
 }
